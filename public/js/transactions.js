9b5e39e1bd1730073d173034c70ec9e107bfae5d
Ricardo Mitsuo Yokota Junior <ricardoyokota01@gmail.com>
2023-03-17 10:39:26 -0300

    bug

--- public/js/transactions.js
+++ public/js/transactions.js
@@ -1,4 +1,33 @@
-const myModal = new bootstrap.Modal("#transactions-modal");
+Skip to content
+Search or jump to…
+Pull requests
+Issues
+Codespaces
+Marketplace
+Explore
+ 
+@RicardoYokota 
+yuribigon
+/
+nikel
+Public
+Fork your own copy of yuribigon/nikel
+Code
+Issues
+Pull requests
+Actions
+Projects
+Security
+Insights
+nikel/public/js/transactions.js /
+@yuribigon
+yuribigon Iniciando o GIT
+Latest commit 320321d on Aug 11, 2022
+ History
+ 1 contributor
+93 lines (72 sloc)  2.38 KB
+
+const myModal = new bootstrap.Modal("#transaction-modal");
 let logged = sessionStorage.getItem("logged");
 const session = localStorage.getItem("session");
 
@@ -8,18 +37,20 @@ let data = {
 
 document.getElementById("button-logout").addEventListener("click", logout);
 
-
-//ADICIONAR LANÇAMENTO
-document.getElementById("transactions-form").addEventListener("submit", function(e) {
+// ADICIONAR LANÇAMENTO
+document.getElementById("transaction-form").addEventListener("submit", function(e) {
     e.preventDefault();
 
-    const value = parseFloat(document.getElementById("value-input").value);
+    const value = parseFloat(document.getElementById("value-input").value); //parseFloat - transforma o número que possa ter virgula
     const description = document.getElementById("description-input").value;
     const date = document.getElementById("date-input").value;
-    const type = document.querySelector('input[name="type-input"]:checked').value;
+    const type = document.querySelector('input[name="type-input"]:checked').value; // pegar o que está checkado
 
-    data.transactions.unshift({
-        value: value, type: type, description: description, date: date
+    data.transactions.unshift ({ // unshift - add o valor na parte de cima da lista
+        value: value,
+        type: type, 
+        description: description, 
+        date: date
     });
 
     saveData(data);
@@ -28,9 +59,7 @@ document.getElementById("transactions-form").addEventListener("submit", function
 
     getTransactions();
 
-        
-    alert("Lançamento realizado com sucesso.");
-
+    alert("Lançamento adicionado com sucesso.");
 });
 
 checkLogged();
@@ -41,26 +70,26 @@ function checkLogged() {
         logged = session;
     }
 
-    if(!logged) {     
+    if(!logged) {        
         window.location.href = "index.html";
+        return;
     }
 
-
     const dataUser = localStorage.getItem(logged);
-    if(dataUser) {
+    if(dataUser){
         data = JSON.parse(dataUser);
     }
 
     getTransactions();
+};
 
-}
-
+// FUNÇÃO LOGOUT
 function logout() {
     sessionStorage.removeItem("logged");
     localStorage.removeItem("session");
 
     window.location.href = "index.html";
-}
+};
 
 function getTransactions() {
     const transactions = data.transactions;
@@ -70,25 +99,39 @@ function getTransactions() {
         transactions.forEach((item) => {
             let type = "Entrada";
 
-            if(item.type ==="2") {
-                type = "Saída;"
+            if(item.type === "2") {
+                type = "Saída";
             }
 
             transactionsHtml += `
-            <tr>
-                <th scope="row">${item.date}</th>
-                <td>${item.value.toFixed(2)}</td>
-                <td>${type}</td>
-                <td>${item.description}</td>
-            </tr>
+                <tr>
+                    <th scope="row">${item.date}</th>
+                    <td>${item.value.toFixed(2)}</td>
+                    <td>${type}</td>
+                    <td>${item.description}</td>
+                </tr> 
             `
         })
     }
 
-    document.getElementById("transcation-list").innerHTML = transactionsHtml;
-
+    document.getElementById("transactions-list").innerHTML = transactionsHtml;
 }
 
 function saveData(data) {
     localStorage.setItem(data.login, JSON.stringify(data));
-}  
+};
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
+nikel/transactions.js at master · yuribigon/nikel
