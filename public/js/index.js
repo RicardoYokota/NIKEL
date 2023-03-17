9c1125fe4a19ec14896c4d931695fe0642b42208
Ricardo Mitsuo Yokota Junior <ricardoyokota01@gmail.com>
2023-03-17 14:21:26 -0300

    ok

--- public/js/index.js
+++ public/js/index.js
@@ -5,7 +5,7 @@ const session = localStorage.getItem("session");
 checkLogged();
 
 //LOGAR NO SISTEMA
-document.getElementById("login-form").addEventListener("submit", function(e) {
+document.getElementById("login-form").addEventListener("submit", function (e) {
     e.preventDefault();
 
     const email = document.getElementById("email-input").value;
@@ -14,13 +14,13 @@ document.getElementById("login-form").addEventListener("submit", function(e) {
 
     const account = getAccount(email);
 
-    if(!account) {
+    if (!account) {
         alert("Ooops! Verifique o e-mail ou a senha.");
         return;
     }
 
-    if(account) {
-        if(account.password !== password) {
+    if (account) {
+        if (account.password !== password) {
             alert("Ooops! Verifique o e-mail ou a senha.");
             return;
         }
@@ -32,40 +32,40 @@ document.getElementById("login-form").addEventListener("submit", function(e) {
 });
 
 //CRIAR CONTA
-document.getElementById("create-form").addEventListener("submit", function(e) {
+document.getElementById("create-form").addEventListener("submit", function (e) {
     e.preventDefault();
 
     const email = document.getElementById("email-create-input").value;
     const password = document.getElementById("password-create-input").value;
 
-   if(email.length < 5) {
-    alert("Preencha o campo com um e-mail válido.");
-    return;
-   }
+    if (email.length < 5) {
+        alert("Preencha o campo com um e-mail válido.");
+        return;
+    }
 
-   if(password.length < 4) {
-    alert("Preencha a senha com no mínimo 4 dígitos.")
-    return;
-   }
+    if (password.length < 4) {
+        alert("Preencha a senha com no mínimo 4 dígitos.")
+        return;
+    }
 
-   saveAccount({
-    login: email,
-    password: password,
-    transactions: []
-   });
+    saveAccount({
+        login: email,
+        password: password,
+        transactions: []
+    });
 
-   myModal.hide();
+    myModal.hide();
 
-   alert("Conta criada com sucesso")
+    alert("Conta criada com sucesso")
 });
 
 function checkLogged() {
-    if(session) {
+    if (session) {
         sessionStorage.setItem("logged", session);
         logged = session;
     }
 
-    if(logged) {
+    if (logged) {
         saveSession(logged, session);
 
         window.location.href = "home.html";
@@ -73,12 +73,12 @@ function checkLogged() {
 }
 
 function saveAccount(data) {
-    localStorage.setItem(data.login,JSON.stringify(data));
+    localStorage.setItem(data.login, JSON.stringify(data));
 }
 
 function saveSession(data, saveSession) {
-    if(saveSession) {
-        localStorage.setItem("session", data) ;
+    if (saveSession) {
+        localStorage.setItem("session", data);
     }
 
     sessionStorage.setItem("logged", data);
@@ -87,7 +87,7 @@ function saveSession(data, saveSession) {
 function getAccount(key) {
     const account = localStorage.getItem(key);
 
-    if(account) {
+    if (account) {
         return JSON.parse(account);
     }
 
